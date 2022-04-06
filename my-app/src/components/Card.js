867f14c5ca5f552a0bdf009632a476f8f6201a19
Abinanthankv <abinanthankv@protonmail.com>
2022-04-06 15:46:11 +0530

    update

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "../styles/Card.css"
 import coimbatore from "../images/coimbatore.jpg"
 export default function Card() {
   return (
     <section id="destinations">
-      <h1>Hello</h1>
-      <img className="slides" src={coimbatore}  ></img>
+      <h1>Places to Visit</h1>
+      <div className="cards">
+      <img  src={coimbatore}  ></img>
+
+
+      </div>
+     
     </section>
   );
 }
